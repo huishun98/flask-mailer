e86dad5bfe8d7c990952637a30cb7ce631182a97
Chua Hui Shun <huishun@dhcp-206-87-127-32.ubcsecure.wireless.ubc.ca>
2019-11-01 22:34:25 -0700

    heroku

---
+++ wgsi.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_mail import Mail, Message
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+load_dotenv()
+
+app.config.update(
+    DEBUG=True,
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USE_TLS=False,
+    MAIL_USERNAME=getenv('MAIL_USERNAME', None),
+    MAIL_PASSWORD=getenv('MAIL_PASSWORD', None),
+)
+
+mail = Mail(app)
+
+@app.route('/', methods=['POST'])
+def index():
+    msg = Message('New message from Portfolio website',
+                  sender='huishunchua@gmail.com',
+                  recipients=[getenv('MAIL_USERNAME', None)])
+    msg.body = 'huehue'
+    mail.send(msg)
+    return 'Message sent!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
